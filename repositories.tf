b20437494b6ebd916d529c442276bea0ce3b8d53
Scott R Bowe <scottb@sentania.net>
2025-12-10 14:50:41 -0600

    make it simple to start

--- repositories.tf
+++ repositories.tf
@@ -1,43 +1,8 @@
-locals {
-  repo_defs = {
-    for k, p in module.projects :
-    k => {
-      project_name = p.project_name
-      project_id   = p.id
-
-      project_slug = trim(
-        regexreplace(
-          lower(p.project_name),
-          "[^a-z0-9]+",
-          "-"
-        ),
-        "-"
-      )
-
-      project_id_short = substr(p.id, 0, 12)
-
-      project_key = format(
-        "%s__%s",
-        trim(
-          regexreplace(
-            lower(p.project_name),
-            "[^a-z0-9]+",
-            "-"
-          ),
-          "-"
-        ),
-        substr(p.id, 0, 12)
-      )
-    }
-  }
-}
-
-
 module "repository" {
   source  = "mineiros-io/repository/github"
   version = "0.18.0"
 
-  for_each = local.repo_defs
+  for_each = [module.projects]
 
   name       = each.value.project_key
   visibility = "public"
@@ -47,8 +12,7 @@ module "repository" {
   }
 
   plaintext_secrets = {
-    VCFA_PROJECT_NAME      = each.value.project_name
-    VCFA_PROJECT_KEY       = each.value.project_key
+    VCFA_PROJECT_NAME      = replace(each.value.name, " ", "_")
     VCFA_PROJECT_ID        = each.value.id
     VCFA_ORGANIZATION_NAME = var.vcfa_organization
   }
